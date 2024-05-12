4168c0928f1d6319e97a8bee74b7df5f45a7b3d6
Nic Moe <NicMoe@users.noreply.github.com>
2024-05-11 19:23:19 -0700

    Temp color scheme changes to distinguish site

--- utils/theme.utils.js
+++ utils/theme.utils.js
@@ -1,10 +1,10 @@
 const theme = {
-  bg01: '#131416',
-  bg02: '#1f2123',
-  bg03: '#25272A',
+  bg01: '#76B041',
+  bg02: '#191970',
+  bg03: '#003366',
   text01: '#F0F6FA',
   text02: '#C1D4E1',
-  primary: '#65F4AD',
+  primary: '#ffc669',
 }
 
 export default theme
