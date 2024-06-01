c644f32b14d68820fb7095308be1ff255c48e93e
Nic Moe <NicMoe@users.noreply.github.com>
2024-06-01 09:04:03 -0700

    Dropped green

--- components/GlobalStyles.jsx
+++ components/GlobalStyles.jsx
@@ -14,7 +14,7 @@ export default createGlobalStyle`
   :root {
     --theme-bg-01: hsl(49, 90%, 95%);
     --theme-text-01: hsl(0deg 0% 0%);
-    --theme-bg-02: hsl(182, 70%, 30%);
+    --theme-bg-02: hsl(213deg, 70%, 9%);
     --theme-text-02: hsl(0 0% 100%);
     --theme-bg-03: hsl(27.78 80% 50%);
     --theme-text-03: hsl(0 0% 100%);
