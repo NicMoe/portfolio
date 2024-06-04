4b66f517c3ce4ced00fca18fd5daed652c06a6ac
Nic Moe <NicMoe@users.noreply.github.com>
2024-06-04 11:44:05 -0700

    Renamed posts to articles

--- components/ProjectCardsGrid.jsx
+++ components/ProjectCardsGrid.jsx
@@ -57,7 +57,7 @@ export default function ProjectCardsGrid({
     }
   }, [router.query]);
 
-  const typeFilters = ['project', 'post'];
+  const typeFilters = ['project', 'article'];
 
   const handleTypeFilterChange = type => {
     setSelectedType(type)
